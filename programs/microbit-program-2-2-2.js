cb0917f316d9c8aacd91c322776b371aee397239
msudenver1040 <miwakami@msudenver.edu>
2025-09-04 20:47:54 -0600

    Create microbit-program-2-2-2.js

---
+++ programs/microbit-program-2-2-2.js
@@ -0,0 +1,7 @@
+/* 
+   This program displays "Hi" on the micro:bit. 
+   It demonstrates the use of block and inline comments.
+*/
+
+// Step 2: One-line program with comments
+basic.showString("Hi");   // Shows "Hi" because it’s a simple greeting
